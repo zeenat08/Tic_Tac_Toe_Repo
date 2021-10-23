6754d368e30236620e829e7e6d9be170f28d3082
zeenat08 <zeenatkhanbfs@gmail.com>
2021-10-23 09:31:33 +0530

    ZEENAT: [added] main method

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tic_Tac_Toe
+{
+    class Program
+    {
+        static public void Main(String[] args)
+        {
+            {
+                Creating_Board obj = new Creating_Board();
+                Console.WriteLine("***************WELCOME TO TIC-TAC-TOE GAME***************");
+                obj.createGameBoard();
+
+            }
+
+        }
+    }
+}
+
+
+
