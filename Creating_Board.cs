1ae97edf776e15fb829475e2e09f272c366b76d8
zeenat08 <zeenatkhanbfs@gmail.com>
2021-10-23 09:27:09 +0530

    ZEENAT: [added] creation of board of tic tac toe

---
+++ Creating_Board.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tic_Tac_Toe
+{
+    public class Creating_Board
+        {
+
+            
+            char[] gameBoard = new char[10];
+
+        internal void showBoard()
+        {
+            throw new NotImplementedException();
+        }
+
+        //static char[] gameBoard = {' ',' ' };
+
+        //method for creating empty GameBoard        
+        public void createGameBoard()
+            {
+
+                for (int a = 1; a < gameBoard.Length; a++)
+                {
+                    gameBoard[a] = ' ';
+                }
+
+                Console.WriteLine("  " + gameBoard[1] + " | " + gameBoard[2] + " | " + gameBoard[3] + "  ");
+                Console.WriteLine("----|---|----");
+                Console.WriteLine("  " + gameBoard[4] + " | " + gameBoard[5] + " | " + gameBoard[6] + "  ");
+                Console.WriteLine("----|---|----");
+                Console.WriteLine("  " + gameBoard[7] + " | " + gameBoard[8] + " | " + gameBoard[9] + "  ");
+        }
+    }
+}
+
+
