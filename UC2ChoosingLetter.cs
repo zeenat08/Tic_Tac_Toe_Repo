686f34fc44001396ff8275c4ffcd934195ab0387
zeenat08 <zeenatkhanbfs@gmail.com>
2021-10-23 10:37:52 +0530

    ZEENAT: added choosing letter of game tic tac toe

---
+++ UC2ChoosingLetter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tic_Tac_Toe
+{
+    class UC2ChoosingLetter
+    {
+         //declare 1d array of char type
+
+        char[] gameBoard = new char[10];
+
+        char Computerletter;
+        char Playerletter;
+
+        //method for creating empty GameBoard
+        public void CreateGameBoard()
+        {
+            for (int a = 1; a < gameBoard.Length; a++)
+            {
+                gameBoard[a] = ' ';
+            }
+        }
+
+        //method for choose Letter X or O
+        public void selectLetter()
+        {
+            Console.WriteLine("\nChoose your Letter\nPress 1 For-> 'X'\nPress 2 For -> 'O'\nEnter your Choice -> ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            if (choice == 1)
+            {
+                Playerletter = 'X';
+                Computerletter = 'O';
+                Console.WriteLine("\nPlayer letter -> " + Playerletter);
+                Console.WriteLine("Computer letter -> " + Computerletter);
+            }
+            else if (choice == 2)
+            {
+                Playerletter = 'O';
+                Computerletter = 'X';
+                Console.WriteLine("\nPlayer letter -> " + Playerletter);
+                Console.WriteLine("Computer letter -> " + Computerletter);
+            }
+            else
+            {
+                Console.WriteLine("\n>>>>>>> plz enter 1 or 2 <<<<<<<<<");
+            }
+        }
+    }
+}
+    
