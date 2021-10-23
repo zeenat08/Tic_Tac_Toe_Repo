c87d495493755428e2ec4b1dd3bd4195213c82d5
zeenat08 <zeenatkhanbfs@gmail.com>
2021-10-23 12:08:01 +0530

    ZEENAT: added main method

--- Program.cs
+++ Program.cs
@@ -11,11 +11,25 @@ namespace Tic_Tac_Toe
                 Console.WriteLine("***************WELCOME TO TIC-TAC-TOE GAME***************");
                 obj.createGameBoard();
 
-            }
 
+                UC2ChoosingLetter choosing_Letter = new UC2ChoosingLetter();
+                choosing_Letter.CreateGameBoard();
+                choosing_Letter.selectLetter();
+
+                
+
+                UC3Showing_Board obj2 = new UC3Showing_Board();
+                obj2.CreateGameBoard();
+                obj2.selectLetter();
+                obj2.showBoard();
+
+
+            }
         }
     }
 }
+
+
 
 
 
